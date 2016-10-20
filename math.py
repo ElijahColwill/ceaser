6a2e36d25a5311ff1662073d2d152ad10dfaa7e1
ElijahColwill <elijah.c035@gmail.com>
2016-10-20 13:58:14 -0600

    Update math.py

--- math.py
+++ math.py
@@ -1,1 +1,8 @@
+fruit = ['apples', 'pears', 'kiwis', 'cherries', 'oranges']
+meals = ['breakfast', 'lunch', 'dinner']
 
+i = 0
+for f in fruit:
+    meal = meals[i % len(meals)]
+    print f, "for", meal
+    i = i + 1
