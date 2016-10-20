618be6d351e1f1026eeb61b1c34977f83a9a9817
ElijahColwill <elijah.c035@gmail.com>
2016-10-20 13:57:49 -0600

    Update ceaser.py

--- ceaser.py
+++ ceaser.py
@@ -1,1 +1,17 @@
+def encode_character(character, key):
+    ascii_code = ord(character)
+    number = ascii_code - ord('a')
+    number = (number - key) % 26
+    number += ord('a')
+    return chr(number)
 
+def encode(message, key):
+    cypher_text = ""
+
+    for char in message:
+        cypher_text += encode_character(char, key)
+
+    return cypher_text
+
+print encode('dbu', 1) # cat
+print encode('dssoh', 3) # apple
